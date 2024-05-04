814238708411addc1ebf6303cebf3ec11cad73b4
ayeraKhan <contactayera@gmail.com>
2024-05-04 13:45:50 +0500

    The Project creates a To-Do React app with backend connectivity with MongoDB

---
+++ WebProg/src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import './App.css'
+import Home from './Home'
+
+function App() {
+
+  return(
+    <div className="container">
+    <div className="home">
+      <Home />
+    </div>
+    </div>
+  )
+}
+
+export default App
